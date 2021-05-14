b177ef376c1cb3e8a0761f99f45750c8688e8b4d
lionel <lionel.faveur@gmail.com>
2021-05-14 22:41:09 +0200

    gdfhgbvfdhb

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,7 +13,8 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        $i = "gfrdgd" . "gdfgbvdfgb";
+        $i = 'gfrdgd' . 'gdfgbvdfgb';
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
